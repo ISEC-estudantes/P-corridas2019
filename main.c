73cf06c7dc0e3a29cb1a61ed8799e9e0562c32da
Oniken <superjmm123@gmail.com>
2019-05-31 21:42:00 +0100

    Correção do menu final dos treinos
    corrigiu se o problema de alguns scanfs n terem um & para as variaveis

--- main.c
+++ main.c
@@ -10,37 +10,49 @@
 #include "utils.h"
 #include "menus.h"
 
+psS inicializa()
+{
+  initRandom();
 
-
-int main(int argc, char** argv) {
-
-   initRandom();
-    
   // ler ficheiros de texto dos pilotos e carros e guardar as suas variaveis
-    //ler pilotos
+  //ler pilotos
   pPi pPilotos = NULL;
-  int nPilotos = 0, erro=0;
+  int nPilotos = 0, erro = 0;
   erro = readPilotos(PILOTOS, &pPilotos, &nPilotos);
   if (erro != 0)
-    return erro;
-    
+    return NULL;
+
   pCar pCarros;
   int nCarros;
   erro = readCar(CARROS, &pCarros, &nCarros);
-  if (erro != 0){
+  if (erro != 0)
+  {
     free(pPilotos);
-    return erro;
+    return NULL;
   }
-   
+
   //criar estrutura para gerir tudo
   psS saveS = makegeral(pPilotos, nPilotos, pCarros, nCarros);
-  if(saveS==NULL){
+  if (saveS == NULL)
+  {
     free(pPilotos);
     free(pCarros);
-    return erro;
+    return NULL;
   }
-//corridamenu(saveS);
-  mainmenu(saveS);
+  return saveS;
+}
 
-  return erro;
-  }
+
+int main(int argc, char **argv)
+{
+
+  psS saveS = inicializa();
+  if (saveS == NULL)
+    return 1;
+
+   corridamenu( saveS, 5, 500, 10);
+  //mainmenu(saveS);
+
+  return 0;
+}
+
